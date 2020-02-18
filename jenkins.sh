54534cad1e5b6a0ca602a4a01ffe74870835e0a0
GravesJake <jacob@greatscottgadgets.com>
2020-02-18 13:25:12 -0700

    jenkins: add freestyle build script

---
+++ jenkins.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+GIT_COMMITER_NAME="CI Person"
+GIT_COMMITER_EMAIL="ci@greatscottgadgets.com"
+python3 -m venv env
+. env/bin/activate
+pip3 install --upgrade pip
+pip3 install PyYAML
+pushd libgreat/host/
+python3 setup.py build
+python3 setup.py install
+popd
+pushd host/
+python3 setup.py build
+python3 setup.py install
+popd
+make firmware
+deactivate
